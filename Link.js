b446f43667cb89cfb33a532f4b929ea58d709de3
twinh <twinhuang@qq.com>
2020-02-16 20:13:38 +0800

    feat: 拆分出 Link 组件

---
+++ Link.js
@@ -0,0 +1,20 @@
+import React from "react";
+import {Link as RouterLink} from "react-router-dom";
+import {ModalContext} from '@miaoxing/router-modal';
+import {createLocation} from 'history';
+
+export default function Link({to, modal, autoModal, ...props}) {
+  const location = createLocation(to, {modal});
+
+  if (!autoModal) {
+    return <RouterLink to={location} {...props}/>
+  }
+
+  // 分开减少层级和逻辑
+  return <ModalContext.Consumer>
+    {({isModal}) => {
+      location.state.modal = modal || (isModal && autoModal);
+      return <RouterLink to={location} {...props}/>
+    }}
+  </ModalContext.Consumer>
+}
