88d34a513d4a966366a47f39412113cfe1435818
twinh <twinhuang@qq.com>
2020-08-14 17:12:33 +0800

    style(eslint): single quotes, double jsx-quotes

--- Link.js
+++ Link.js
@@ -1,5 +1,5 @@
-import React from "react";
-import {Link as RouterLink} from "react-router-dom";
+import React from 'react';
+import {Link as RouterLink} from 'react-router-dom';
 import {ModalContext} from '@mxjs/router-modal';
 import {createLocation} from 'history';
 
