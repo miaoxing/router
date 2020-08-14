f8da30a51b63acc64bb38e2050a3aa18d141a935
twinh <twinhuang@qq.com>
2020-08-14 17:23:35 +0800

    style(eslint): semi: always

--- Link.js
+++ Link.js
@@ -7,14 +7,14 @@ export default function Link({to, modal, autoModal, ...props}) {
   const location = createLocation(to, {modal});
 
   if (!autoModal) {
-    return <RouterLink to={location} {...props}/>
+    return <RouterLink to={location} {...props}/>;
   }
 
   // 分开减少层级和逻辑
   return <ModalContext.Consumer>
     {({isModal}) => {
       location.state.modal = modal || (isModal && autoModal);
-      return <RouterLink to={location} {...props}/>
+      return <RouterLink to={location} {...props}/>;
     }}
-  </ModalContext.Consumer>
+  </ModalContext.Consumer>;
 }
